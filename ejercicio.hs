2a907dc0c192b6a78ff4fb5c019557d065080c7e
vpennella <vpennella@gmail.com>
2018-03-23 21:27:38 -0300

    Subir ejercicio
    Subimos clase de lab viernes noche

---
+++ ejercicio.hs
@@ -0,0 +1,45 @@
+
+suma :: Integer -> Integer -> Integer
+suma nro otro = nro + otro
+
+and' :: Bool -> Bool -> Bool
+and' exp otraExp | exp  = otraExp
+                  | otherwise = False
+
+and'' :: Bool -> Bool -> Bool
+and'' True exp = exp
+and'' _  _ = False
+
+or'' :: Bool -> Bool -> Bool
+or'' False False = False
+or'' _ _ = True
+
+or' :: Bool -> Bool -> Bool
+or' exp otraExp | exp = True
+                 |otherwise = otraExp
+
+signo :: Integer -> Integer
+signo 0 = 0
+signo nro | nro > 0 = 1
+           | otherwise = -1
+
+
+data Persona = Gimnasta { nombre :: String, fuerza :: Integer} deriving Show
+
+
+vertical :: Persona -> Persona
+vertical pers = pers { fuerza = fuerza pers + 10}
+
+julia :: Persona
+julia = Gimnasta "julia" 10
+
+pedro :: Persona
+pedro = Gimnasta "pedro" 8
+
+
+
+
+
+
+
+
