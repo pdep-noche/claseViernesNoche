d6eb9f960b49ad4093d2062bcfca1a179917abda
vpennella <vpennella@gmail.com>
2018-03-24 21:43:29 -0300

    Agregamos comentarios
    Se agregaron comentarios a las funciones

--- ejercicio.hs
+++ ejercicio.hs
@@ -24,9 +24,9 @@ signo nro | nro > 0 = 1
            | otherwise = -1
 
 
-data Persona = Gimnasta { nombre :: String, fuerza :: Integer} deriving Show
+data Persona = Gimnasta { nombre :: String, fuerza :: Integer}
 
-
+--Cuando hace la vertical incrementa la fuerza en 10 unidades
 vertical :: Persona -> Persona
 vertical pers = pers { fuerza = fuerza pers + 10}
 
@@ -36,17 +36,13 @@ julia = Gimnasta "julia" 10
 pedro :: Persona
 pedro = Gimnasta "pedro" 8
 
---aplica :: (Integer -> Integer) -> (Integer, Integer, Integer) -> (Integer, Integer, Integer)
+aplica :: (Integer -> Integer) -> (Integer, Integer, Integer) -> (Integer, Integer, Integer)
 aplica f (num, otro, otroNum) = (f num, f otro, f otroNum)
 
 
 suma''':: (Integer, Integer, Integer) -> (Integer, Integer, Integer)
 suma''' (x, y, z) = (2*x, y, z)
 
-
-
-
-
-
-
-
+-- ejemplo de composición podría ser:
+-- (aplica (suma 5) . suma''') (3,4,7)
+-- (11,9,12)
